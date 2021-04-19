2ea7a228bbd2f26c4ae8f8eb8790473820f7b73d
Dhanvi31 <79754778+Dhanvi31@users.noreply.github.com>
2021-04-19 12:10:01 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,54 +1,59 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj, paperObject,groundObject	
+var world;
 
-var paper
-var ground
-var dustbin1
-var dustbin2
-var dustbin3
 
 function setup() {
-	canvas = createCanvas(1200,400);
+	createCanvas(1600, 700);
+	rectMode(CENTER);
 
 
 	engine = Engine.create();
 	world = engine.world;
+	dustbinObj=new dustbin(1200,650);
+	paperObject=new paper(200,450,40);
+	groundObject=new ground(width/2,670,width,20);
+	//Create a Ground
+	
 
-	//Create the Bodies Here.
-
-     paper= new Paper(100,200,45) 
-  
-	 ground=new Ground(300,390,1900,10)
-
-	 dustbin1=new Dustbin(width/1.4,height-45,10,60)
-	 dustbin2=new Dustbin(width/1.2,height-45,10,60)
-	 dustbin3=new Dustbin(width/1.292,height-20,130,10)
-
-
+	var render = Render.create({
+	  element: document.body,
+	  engine: engine,
+	  options: {
+	    width: 1200,
+	    height: 700,
+	    wireframes: false
+	  }
+	});
 
 	Engine.run(engine);
+	//Render.run(render);
   
 }
 
 
 function draw() {
+  rectMode(CENTER);
+  background(0);
+ 
+  dustbinObj.display();
+  paperObject.display();
+  groundObject.display();
   
-	Engine.update(engine);
-
-  background("lightBlue");
+ 
   
-  paper.display()
-ground.display()
-
-dustbin1.display()
-dustbin2.display()
-dustbin3.display()
- }
-function keyPressed(){
-	if(keyCode === UP_ARROW){
-Matter.Body.applyForce(paper.body,paper.body.position,{x:100,y:-100})
+  
+ 
 }
+
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
+
+    	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+    
+  	}
 }
